de185d728c290bf439ff8de26ee95546463301a3
hj5o <dataliteracy@naver.com>
2022-08-03 20:56:47 +0900

    Initial commit

---
+++ practice1/src/test/java/hj5o/board/practice1/Practice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hj5o.board.practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
