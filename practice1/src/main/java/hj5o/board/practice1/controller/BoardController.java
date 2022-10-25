eafa820863785fede5bd91e004d40900b423f5f6
hj5o <dataliteracy@naver.com>
2022-10-25 21:42:52 +0900

    게시글 생성 및 삭제

--- practice1/src/main/java/hj5o/board/practice1/controller/BoardController.java
+++ practice1/src/main/java/hj5o/board/practice1/controller/BoardController.java
@@ -4,6 +4,7 @@ import hj5o.board.practice1.entity.Board;
 import hj5o.board.practice1.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,14 +17,30 @@ public class BoardController {
 
     @GetMapping("/board/form")
     public String form() {
-        return "board/Form";
+        return "board/form";
     }
 
     @PostMapping("/board/form/process")
     public String formProcess(Board board) {
-
         boardService.trans(board);
-
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+        return "board/list";
+    }
+
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "board/view";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
     }
 }
--- practice1/src/main/java/hj5o/board/practice1/service/BoardService.java
+++ practice1/src/main/java/hj5o/board/practice1/service/BoardService.java
@@ -5,6 +5,8 @@ import hj5o.board.practice1.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -13,5 +15,19 @@ public class BoardService {
 
     public void trans(Board board) {
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    public Board boardView (Integer id) {
+        return boardRepository.findById(id).get();
+    }
+
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
     }
 }
