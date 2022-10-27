cd8647c5cd6dd9a5da71ad1b80ffec9ec65cec4b
hj5o <dataliteracy@naver.com>
2022-10-27 23:58:53 +0900

    게시글 수정

--- practice1/src/main/java/hj5o/board/practice1/controller/BoardController.java
+++ practice1/src/main/java/hj5o/board/practice1/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,7 +24,7 @@ public class BoardController {
     @PostMapping("/board/form/process")
     public String formProcess(Board board) {
         boardService.trans(board);
-        return "";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
@@ -36,6 +37,20 @@ public class BoardController {
     public String boardView(Model model, Integer id) {
         model.addAttribute("board", boardService.boardView(id));
         return "board/view";
+    }
+
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "board/update";
+    }
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/delete")
